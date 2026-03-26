dc77650ee491029ef0b35321d1bb6421aaa983a9
PATIENCE KANDARE <PATIENCE.KANDARE@HOTMAIL.COM>
2026-03-26 13:53:03 +0000

    read me

--- src/app/api/save-meal/route.js
+++ src/app/api/save-meal/route.js
@@ -1,9 +1,13 @@
 import pool from "@/utils/db/db";
+import { revalidatePath } from "next/cache";
 
 export async function POST(request) {
+    try {
     const { userId, mealId } = await request.json();
 
-    try {
+     if (!userId || !mealId) {
+      return Response.json();
+    }
         await pool.query(`
             INSERT INTO user_saves (user_id, meal_id)
             VALUES ($1, $2)
@@ -11,6 +15,8 @@ export async function POST(request) {
         `,
         [userId, mealId]
     );  
+
+    revalidatePath("/saved");
        
     return Response.json({ success: true});
     }   catch (error) {
--- src/app/components/SaveButton.jsx
+++ src/app/components/SaveButton.jsx
@@ -5,8 +5,12 @@ import { Bookmark } from "lucide-react";
 
 export default function SaveButton({ mealId}) {
     const [saved, setSaved] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     async function handleSave() {
+        try {
+        setLoading(true);
+
         const response = await fetch("/api/save-meal", {
             headers: {
             "Content-Type" : "application/json"
@@ -23,17 +27,28 @@ export default function SaveButton({ mealId}) {
     if (data.success) {
     setSaved(true);
         }
+    } finally {
+        setLoading(false);
+        }
     }
 
          return (
-            <button onClick={handleSave}>
+            <button onClick={handleSave}
+            disabled={loading}
+            style={{
+            background: "transparent",
+            border: "none",
+            cursor: "pointer",
+            padding: 0,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+      }}
+            >
              <Bookmark
                 size={22}
-                className={`transition ${
-                    saved
-                    ? "fill-white text-white"
-                    : "text-white/70 hover:text-white"
-                }`}
+                color={saved ? "white" : "rgba(255,255,255,0.7)"}
+                fill={saved ? "currentColor" : "none"}
             />   
             </button>
          );
